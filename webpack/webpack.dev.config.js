83ec411ebed6550772b0e7dc2d5cbec16bed18c0
Manoj Singh Negi <me@manojsinghnegi.com>
2017-08-21 23:29:39 +0530

    replaced tabs with spaces

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -4,21 +4,21 @@ var path = require('path');
 var parentDir = path.join(__dirname, '../');
 
 module.exports = {
-	entry: [
-		path.join(parentDir, 'index.js')
-	],
-	module: {
-		loaders: [{
-			test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},{
-				test: /\.less$/,
-				loaders: ["style-loader", "css-loder", "less-loader"]
-			}
-		]
-	},
-	output: {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
+    },
+    output: {
         path: parentDir + '/dist',
         filename: 'bundle.js'
     },
