bc73be75b8e5a1d52dff385649c696766362f503
Manoj Singh Negi <me@manojsinghnegi.com>
2017-08-25 11:12:53 +0530

    added redux

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 export default class App extends Component {
-	render () {
-		return <p>This is my new react app</p>
-	}
+    render () {
+        return <p>This is my new react app</p>
+    }
 }
