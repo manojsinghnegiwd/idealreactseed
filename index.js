dfe895a21fe7ce4326f9711200668ee89de7e0ca
Manoj Singh Negi <me@manojsinghnegi.com>
2017-08-26 11:46:37 +0530

    added provider in index

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ import ReactDOM from 'react-dom';
 // main app
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+	<Provider>
+		<App />
+	</Provider>,
+	document.getElementById('app')
+)
--- store.js
+++ store.js
@@ -1,11 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { routerReducer } from 'react-router-redux'
 import messageReducer from './reducers/message'
 import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
-  messageReducer,
-  routing: routerReducer
+  messageReducer
 })
 const store = createStore(
   reducer,
