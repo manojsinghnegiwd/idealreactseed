ee585dc14abb3f0ff521ad851de62fadad4e1241
Manoj Singh Negi <me@manojsinghnegi.com>
2017-08-26 12:11:14 +0530

    fixed typos

---
+++ components/InputPreview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class InputPreview extends React.Component {
+	render () {
+		return (
+			<div>
+				<input
+					type="text"
+					/>
+			</div>
+		)
+	}
+}
